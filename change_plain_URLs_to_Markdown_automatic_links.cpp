b3ea7f787b5e257aa4e8d02672d0ead1423e65cd
oronymzy <oronymzy@mostserio.us>
2019-04-25 22:08:48 -0400

    Move code, add code
    Move variable definition to correct location.
    Add code setting the file line count to zero.
    Add three-clause if-construct to detect if a file has a single line or
    has multiple lines.

--- change_plain_URLs_to_Markdown_automatic_links.cpp
+++ change_plain_URLs_to_Markdown_automatic_links.cpp
@@ -23,11 +23,13 @@ char continue_or_exit_program();
 int main()
 {
   // Constant definition
-  int file_line_count = 0;
   const string URL_COMPONENT_1 = "://";
   const string URL_COMPONENT_2 = ".";
   const string URL_COMPONENT_3 = " ";
   const string TWO_CONSECUTIVE_SPACES = "  ";
+
+  // Variable definition
+  int file_line_count = 0;
 
   // Vector definition
   vector<string> markdown_converted_file_contents;
@@ -53,6 +55,9 @@ int main()
     // Opening a file for transput
     link_conversion_file.open(link_conversion_filename, ios::in);
     
+    // Setting the file line count to zero
+    file_line_count = 0;
+    
     // Calculation of the file's line count
     while (std::getline(link_conversion_file, line_of_file))
     {
@@ -62,68 +67,77 @@ int main()
     link_conversion_file.close();
     link_conversion_file.open(link_conversion_filename, ios::in | ios::out);
 
-    // Variable definition
-    int plain_url_found_count = 0;
-    int two_consecutive_trailing_spaces_found_count = 0;
-    int current_line_number = 0;
-    
-    // Iteration through each line of a text file, detecting any plain URLs and changing them into Markdown automatic links. All lines are added to a vector as string elements, regardless of whether they were changed or not.
-    while (std::getline(link_conversion_file, line_of_file))
+    if (file_line_count == 1)
     {
-      current_line_number += 1;
-      // Removing two consecutive trailing spaces from any lines that have them
-      if (line_of_file.length() >= TWO_CONSECUTIVE_SPACES.length() && line_of_file.find(TWO_CONSECUTIVE_SPACES, (line_of_file.length() - TWO_CONSECUTIVE_SPACES.length())) != std::string::npos)
-      {
-        two_consecutive_trailing_spaces_found_count += 1;
-        cout << "Two consecutive trailing spaces were found on line " << current_line_number << "." << endl;
-        modified_line_of_file_with_two_consecutive_trailing_spaces = line_of_file.erase((line_of_file.length() - TWO_CONSECUTIVE_SPACES.length()),  TWO_CONSECUTIVE_SPACES.length());
-        line_of_file = modified_line_of_file_with_two_consecutive_trailing_spaces;
-      }
-      // Adding inequality signs to the beginning and end of plain URLs
-      if (line_of_file.find(URL_COMPONENT_1) != std::string::npos && line_of_file.find(URL_COMPONENT_2) != std::string::npos && line_of_file.find(URL_COMPONENT_3) == std::string::npos && line_of_file.rfind("<", 0) == std::string::npos && line_of_file.find(">", 0) == std::string::npos)
-      {
-        plain_url_found_count += 1;
-        cout << "A plain URL was found on line " << current_line_number << "." << endl;
-        // Adding inequality signs to beginning and end of the current line to change plain URL into Markdown automatic link
-        modified_line_of_file_with_plain_url = "<" + line_of_file + ">";
-        line_of_file = modified_line_of_file_with_plain_url;
-      }
-      // Adding string elements to “markdown_converted_file_contents” vector
-      markdown_converted_file_contents.push_back(line_of_file);
+      cout << "The file contains only one line, and was not modified." << endl;
     }
-
-    link_conversion_file.close();
-    link_conversion_file.open(link_conversion_filename, ios::in | ios::out);
-    
-    // Adding two consecutive trailing spaces to lines meeting specific criteria: do not add two consecutive trailing spaces to lines followed by a blank line, or to blank lines. The temporary “modified_markdown_converted_file_contents” vector is used for this.
-    for (int loop_counter = 0 ; loop_counter <= file_line_count ; loop_counter++)
+    else if (file_line_count > 1)
     {
-      if (!(markdown_converted_file_contents[loop_counter].empty() == false && markdown_converted_file_contents[loop_counter + 1].empty() == true) && markdown_converted_file_contents[loop_counter].empty() == false)
+      // Variable definition
+      int plain_url_found_count = 0;
+      int two_consecutive_trailing_spaces_found_count = 0;
+      int current_line_number = 0;
+      
+      // Iteration through each line of a text file, detecting any plain URLs and changing them into Markdown automatic links. All lines are added to a vector as string elements, regardless of whether they were changed or not.
+      while (std::getline(link_conversion_file, line_of_file))
       {
-        // cout << "Line " << loop_counter + 1 << endl;
-        modified_markdown_converted_file_contents.push_back((markdown_converted_file_contents[loop_counter] + "  "));
+        current_line_number += 1;
+        // Removing two consecutive trailing spaces from any lines that have them
+        if (line_of_file.length() >= TWO_CONSECUTIVE_SPACES.length() && line_of_file.find(TWO_CONSECUTIVE_SPACES, (line_of_file.length() - TWO_CONSECUTIVE_SPACES.length())) != std::string::npos)
+        {
+          two_consecutive_trailing_spaces_found_count += 1;
+          cout << "Two consecutive trailing spaces were found on line " << current_line_number << "." << endl;
+          modified_line_of_file_with_two_consecutive_trailing_spaces = line_of_file.erase((line_of_file.length() - TWO_CONSECUTIVE_SPACES.length()),  TWO_CONSECUTIVE_SPACES.length());
+          line_of_file = modified_line_of_file_with_two_consecutive_trailing_spaces;
+        }
+        // Adding inequality signs to the beginning and end of plain URLs
+        if (line_of_file.find(URL_COMPONENT_1) != std::string::npos && line_of_file.find(URL_COMPONENT_2) != std::string::npos && line_of_file.find(URL_COMPONENT_3) == std::string::npos && line_of_file.rfind("<", 0) == std::string::npos && line_of_file.find(">", 0) == std::string::npos)
+        {
+          plain_url_found_count += 1;
+          cout << "A plain URL was found on line " << current_line_number << "." << endl;
+          // Adding inequality signs to beginning and end of the current line to change plain URL into Markdown automatic link
+          modified_line_of_file_with_plain_url = "<" + line_of_file + ">";
+          line_of_file = modified_line_of_file_with_plain_url;
+        }
+        // Adding string elements to “markdown_converted_file_contents” vector
+        markdown_converted_file_contents.push_back(line_of_file);
+      }
+      
+      link_conversion_file.close();
+      link_conversion_file.open(link_conversion_filename, ios::in | ios::out);
+      
+      // Adding two consecutive trailing spaces to lines meeting specific criteria: do not add two consecutive trailing spaces to lines followed by a blank line, or to blank lines. The temporary “modified_markdown_converted_file_contents” vector is used for this.
+      for (int loop_counter = 0 ; loop_counter <= file_line_count ; loop_counter++)
+      {
+        if (!(markdown_converted_file_contents[loop_counter].empty() == false && markdown_converted_file_contents[loop_counter + 1].empty() == true) && markdown_converted_file_contents[loop_counter].empty() == false)
+        {
+          // cout << "Line " << loop_counter + 1 << endl;
+          modified_markdown_converted_file_contents.push_back((markdown_converted_file_contents[loop_counter] + "  "));
+        }
+        else
+          modified_markdown_converted_file_contents.push_back(markdown_converted_file_contents[loop_counter]);
+      }
+      
+      // Clearing contents of “markdown_converted_file_contents” vector and adding string elements to it from temporary “modified_markdown_converted_file_contents” vector
+      markdown_converted_file_contents.clear();
+      for (int loop_counter = 0 ; loop_counter <= file_line_count ; loop_counter++)
+        markdown_converted_file_contents.push_back(modified_markdown_converted_file_contents[loop_counter]);
+
+      if (plain_url_found_count > 0)
+      {
+        cout << plain_url_found_count << " plain URLs were changed in " << link_conversion_filename << ". " << two_consecutive_trailing_spaces_found_count << " lines had two consecutive trailing spaces removed in " << link_conversion_filename << "." << endl;
+        // Iteration through string elements of a vector, overwriting existing contents of text file with contents of vector.
+        for (vector<string>::const_iterator loop_counter = markdown_converted_file_contents.begin() ; loop_counter != markdown_converted_file_contents.end() ; loop_counter++)
+          link_conversion_file << *loop_counter << "\n";
       }
       else
-        modified_markdown_converted_file_contents.push_back(markdown_converted_file_contents[loop_counter]);
-    }
-    
-    // Clearing contents of “markdown_converted_file_contents” vector and adding string elements to it from temporary “modified_markdown_converted_file_contents” vector
-    markdown_converted_file_contents.clear();
-    for (int loop_counter = 0 ; loop_counter <= file_line_count ; loop_counter++)
-      markdown_converted_file_contents.push_back(modified_markdown_converted_file_contents[loop_counter]);
-
-    if (plain_url_found_count > 0)
-    {
-      cout << plain_url_found_count << " plain URLs were changed in " << link_conversion_filename << ". " << two_consecutive_trailing_spaces_found_count << " lines had two consecutive trailing spaces removed in " << link_conversion_filename << "." << endl;
-      // Iteration through string elements of a vector, overwriting existing contents of text file with contents of vector.
-      for (vector<string>::const_iterator loop_counter = markdown_converted_file_contents.begin() ; loop_counter != markdown_converted_file_contents.end() ; loop_counter++)
-        link_conversion_file << *loop_counter << "\n";
+        cout << "No plain URLs were found." << endl;
+      
+      // Clearing contents of “markdown_converted_file_contents” vector
+      markdown_converted_file_contents.clear();
     }
     else
-      cout << "No plain URLs were found." << endl;
-    
-    // Clearing contents of “markdown_converted_file_contents” vector
-    markdown_converted_file_contents.clear();
+      cout << "Something went wrong.";
     
     // Closing a file
     link_conversion_file.close();
